d372c1c22c520e6f7a5d859bbae5e4a30ce03e9d
Sakir <s.azimkar1@newcastle.ac.uk>
2025-10-05 21:48:04 +0100

    2.20 complete, updated gitignore to include .env

--- part2/countrydata/src/components/Countries.jsx
+++ part2/countrydata/src/components/Countries.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from 'react'
+import weatherService from '../services/countries'
+
 const Country = ({ country }) => {
+  const [weatherInfo, setWeatherInfo] = useState(null)
+  const [lat, lon] = country.latlng
+
+  useEffect(() => {
+    weatherService
+      .getWeather(lat, lon)
+      .then(weather => setWeatherInfo(weather))
+      .catch(() => {
+        alert('Failed to fetch weather')
+        setWeatherInfo(null)
+      })
+  }, [lat, lon])
+
+  const icon = weatherInfo?.weather?.[0]?.icon
+
   return(
     <div>
       <h1>{country.name.common}</h1>
@@ -16,6 +34,11 @@ const Country = ({ country }) => {
 
       <h1>Flag</h1>
       <img src={country.flags.png} />
+
+      <h1>Weather</h1>
+      <div>Temperature: {weatherInfo?.main?.temp} Celsius</div>
+      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} />
+      <div>Wind: {weatherInfo?.wind?.speed} m/s</div>
     </div>
   )
 }
--- part2/countrydata/src/services/countries.js
+++ part2/countrydata/src/services/countries.js
@@ -1,9 +1,17 @@
 import axios from 'axios'
-const baseURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const countriesURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?'
+
+const WEATHER_KEY = import.meta.env.VITE_WEATHER_KEY
 
 const getAll = () => {
-    const request = axios.get(baseURL)
+    const request = axios.get(countriesURL)
     return request.then(response => response.data)
-}   
+}
 
-export default { getAll }
+const getWeather = (lat, lon) => {
+    const request = axios.get(weatherURL + `lat=${lat}&lon=${lon}&appid=${WEATHER_KEY}&units=metric`)
+    return request.then(response => response.data)
+}
+
+export default { getAll, getWeather }
