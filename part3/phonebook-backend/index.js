ffb5b8e9b8093ed7531a7595b56aee73dce5a72d
Sakir <s.azimkar1@newcastle.ac.uk>
2025-10-09 17:54:40 +0100

    ESLint integration and exercise 3.22 complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 const express = require('express')
 const morgan  = require('morgan')
-const mongoose = require('mongoose')
 
 const Person = require('./models/person')
 const PORT = process.env.PORT
@@ -24,18 +23,16 @@ app.use(express.static('dist'))
 app.use(requestLogger)*/
 
 app.use(morgan(
-    'tiny',
-    { skip: (request, response) => { return request.method === 'POST'}}
-  )
-)
+  'tiny',
+  {skip: (request) => { return request.method === 'POST'}}
+))
 
-morgan.token('data', (req, res) => { return JSON.stringify(req.body) })
+morgan.token('data', (req) => { return JSON.stringify(req.body) })
 
 app.use(morgan(
-    ':method :url :status :res[content-length] - :response-time ms :data',
-    { skip: (request, response) => { return request.method !== 'POST' } }
-  )
-)
+  ':method :url :status :res[content-length] - :response-time ms :data',
+  {skip: (request) => { return request.method !== 'POST' }}
+))
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -43,7 +40,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -56,7 +53,7 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number } = request.body
+  const {name, number} = request.body
 
   Person.findById(request.params.id)
     .then(person => {
@@ -76,7 +73,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -89,15 +86,15 @@ app.post('/api/persons', (request, response, next) => {
     name: body.name,
     number: body.number,
   })
-      
+
   person.save()
     .then(savedPerson => {
       response.json(savedPerson)
     })
-    .catch(error => next(error))      
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   const time = new Date()
   let num = 0
 
@@ -121,13 +118,13 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const errorHandler = (error, request, response, next) => {
+const errorHandler = (error, request, response) => {
   console.error(error.message)
 
   if(error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({error: 'malformatted id'})
   } else if(error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).json({error: error.message})
   }
 }
 
