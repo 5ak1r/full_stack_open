9690fbb6e4089ffc5e9c69f3df5da50753c4d18f
Sakir <s.azimkar1@newcastle.ac.uk>
2025-10-23 16:05:27 +0100

    4.15-4.16 complete

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -13,6 +13,12 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const {username, name, password} = request.body
 
+  if(!username || !password || username.length < 3 || password.length < 3) {
+    return response.status(401).json({
+      error: 'invalid username or password format'
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
